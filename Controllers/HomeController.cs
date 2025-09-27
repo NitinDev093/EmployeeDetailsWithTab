5284c2ca53a5d3c243cf5c4ae49e3791706e5af4
NitinDev093 <nitinsahani093@gmail.com>
2025-09-27 20:50:08 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeDetailsWithTab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult BasicInformation()
+        {
+            return PartialView("_BasicInformation");
+        }
+        //Insert Basic Information
+        public ActionResult InsertBasicInfromation()
+        {
+            return View();
+        }
+
+        public PartialViewResult EducationDetails()
+        {
+            return PartialView("_EducationDetails");
+        }
+        public PartialViewResult ExperianceDetails()
+        {
+            return PartialView("_ExperianceDetails");
+        }
+        public PartialViewResult CTCDetails()
+        {
+            return PartialView("_CTCDetails");
+        }
+        public PartialViewResult FamilyDetails()
+        {
+            return PartialView("_FamilyDetails");
+        }
+        public PartialViewResult AddressDetails()
+        {
+            return PartialView("_AddressDetails");
+        }
+        public PartialViewResult OtherDetails()
+        {
+            return PartialView("_OtherDetails");
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeDetailsWithTab.MvcApplication" Language="C#" %>
